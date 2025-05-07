f0a5ee69909714833825b43ace40c1c184054e8f
JeanBizeul <jean.bizeul@epitech.eu>
2025-05-07 10:42:00 +0200

    ✏️ Fix coding style for point & vec hpp/tpp files

--- include/Math/Point.hpp
+++ include/Math/Point.hpp
@@ -5,53 +5,52 @@
 ** point
 */
 
-#ifndef POINT_HPP_
-    #define POINT_HPP_
+#ifndef MATH_POINT_HPP_
+    #define MATH_POINT_HPP_
     #include <cstddef>
     #include <array>
 
     #include "Vec.hpp"
 
-namespace Math
-{
-    template<std::size_t N>
-    class Point
-    {
-        public:
-            Point();
-            Point(double val);
-            Point(std::array<double, N> vals);
+namespace Math {
+template<std::size_t N>
+class Point {
+ public:
+    Point();
+    explicit Point(double val);
+    explicit Point(std::array<double, N> vals);
 
-            ~Point() = default;
+    ~Point() = default;
 
-            Point<N> operator+(const Vec<N> &vector) const;
-            Point<N> operator-(const Vec<N> &vector) const;
-            Point<N> operator+(const Point<N> &Point) const;
-            Point<N> operator-(const Point<N> &Point) const;
+    Point<N> operator+(const Vec<N> &vector) const;
+    Point<N> operator-(const Vec<N> &vector) const;
+    Point<N> operator+(const Point<N> &Point) const;
+    Point<N> operator-(const Point<N> &Point) const;
 
-            void operator+=(const Vec<N> &vector);
-            void operator-=(const Vec<N> &vector);
+    void operator+=(const Vec<N> &vector);
+    void operator-=(const Vec<N> &vector);
 
-            template<std::size_t P>
-            double get() const;
-            double operator[](std::size_t index) const;
+    template<std::size_t P>
+    double get() const;
+    double operator[](std::size_t index) const;
 
-            double x() const;
-            double y() const;
-            double z() const;
-            double w() const;
+    double x() const;
+    double y() const;
+    double z() const;
+    double w() const;
 
-            double r() const;
-            double g() const;
-            double b() const;
-            double a() const;
+    double r() const;
+    double g() const;
+    double b() const;
+    double a() const;
 
-            double u() const;
-            double v() const;
-        private:
-            std::array<double, N> _arr;
-    };
-}
+    double u() const;
+    double v() const;
+
+ private:
+    std::array<double, N> _arr;
+};
+}  // namespace Math
 
 #include "Point.tpp"
-#endif /* !POINT_HPP_ */
+#endif  // MATH_POINT_HPP_
--- include/Math/Vec.hpp
+++ include/Math/Vec.hpp
@@ -5,61 +5,60 @@
 ** vec
 */
 
-#ifndef VEC_HPP_
-    #define VEC_HPP_
+#ifndef MATH_VEC_HPP_
+    #define MATH_VEC_HPP_
     #include <cstddef>
     #include <array>
 
-namespace Math
-{
-    template<std::size_t N>
-    class Vec
-    {
-        public:
-            Vec();
-            Vec(double val);
-            Vec(std::array<double, N> vals);
+namespace Math {
+template<std::size_t N>
+class Vec {
+ public:
+    Vec();
+    explicit Vec(double val);
+    explicit Vec(std::array<double, N> vals);
 
-            ~Vec() = default;
+    ~Vec() = default;
 
-            double dot(const Vec<N> &other) const;
+    double dot(const Vec<N> &other) const;
 
-            Vec<N> operator+(const Vec<N> &other) const;
-            Vec<N> operator-(const Vec<N> &other) const;
-            Vec<N> operator*(const Vec<N> &other) const;
-            Vec<N> operator/(const Vec<N> &other) const;
+    Vec<N> operator+(const Vec<N> &other) const;
+    Vec<N> operator-(const Vec<N> &other) const;
+    Vec<N> operator*(const Vec<N> &other) const;
+    Vec<N> operator/(const Vec<N> &other) const;
 
-            Vec<N> operator*(double val) const;
-            Vec<N> operator/(double val) const;
+    Vec<N> operator*(double val) const;
+    Vec<N> operator/(double val) const;
 
-            void operator+=(const Vec<N> &other);
-            void operator-=(const Vec<N> &other);
-            void operator*=(const Vec<N> &other);
-            void operator/=(const Vec<N> &other);
+    void operator+=(const Vec<N> &other);
+    void operator-=(const Vec<N> &other);
+    void operator*=(const Vec<N> &other);
+    void operator/=(const Vec<N> &other);
 
-            void operator*=(double val);
-            void operator/=(double val);
+    void operator*=(double val);
+    void operator/=(double val);
 
-            template<std::size_t P>
-            double get() const;
-            double operator[](std::size_t index) const;
+    template<std::size_t P>
+    double get() const;
+    double operator[](std::size_t index) const;
 
-            double x() const;
-            double y() const;
-            double z() const;
-            double w() const;
+    double x() const;
+    double y() const;
+    double z() const;
+    double w() const;
 
-            double r() const;
-            double g() const;
-            double b() const;
-            double a() const;
+    double r() const;
+    double g() const;
+    double b() const;
+    double a() const;
 
-            double u() const;
-            double v() const;
-        private:
-            std::array<double, N> _arr;
-    };
-}
+    double u() const;
+    double v() const;
+
+ private:
+    std::array<double, N> _arr;
+};
+}  // namespace Math
 
 #include "Vec.tpp"
-#endif /* !VEC_HPP_ */
+#endif  // MATH_VEC_HPP_
