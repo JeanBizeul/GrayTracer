94fca7402094c0152fd0aab3e003b2e9e43f940d
ChifuyuOnWish <noah.auroy@epitech.eu>
2025-05-15 20:19:33 +0200

    ✨ add Light and LightInfo classes

---
+++ include/RayTracer/Light.hpp
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2025
+** GrayTracer
+** File description:
+** Light
+*/
+
+#ifndef RAYTRACER_LIGHT_HPP_
+#define RAYTRACER_LIGHT_HPP_
+
+#include <optional>
+
+#include "Math/Point3.hpp"
+#include "Math/Point4.hpp"
+#include "Math/Vec3.hpp"
+
+namespace RayTracer {
+class Light {
+ public:
+    Light(Math::Point3 position, Math::Vec3 direction, double angle,
+          LightInfo lightInfo)
+        : _LightInfo(lightInfo),
+          _position(position),
+          _direction(direction),
+          _angle(angle) {
+    }
+
+    const std::optional<LightInfo> hit(Math::Point3 _impact) const;
+
+ private:
+    Math::Point3 _position;
+    Math::Vec3 _direction;
+    double _angle;
+
+    LightInfo _LightInfo;
+};
+
+class LightInfo {
+ public:
+    LightInfo(Math::Point4 color, double intensity)
+        : _color(color), _intensity(intensity) {
+    }
+
+    Math::Point4 _color;
+    double _intensity;
+};
+
+}  // namespace RayTracer
+
+#endif  // RAYTRACER_LIGHT_HPP_
